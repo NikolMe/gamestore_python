dfce7974c5c6c4e7b2e9a1ab70ab72475318a778
nikolantiukova <antukovan@gmail.com>
2024-04-27 17:25:23 +0300

    add changes

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from games.models import Publisher, Category, Game
 
-# Register your models here.
+
+class GameInstanceInline(admin.TabularInline):
+    model = Game
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    inlines = [GameInstanceInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Game)
+class Game(admin.ModelAdmin):
+    list_filter = ['categories', 'publisher']
+    pass
